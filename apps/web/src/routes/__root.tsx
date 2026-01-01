0826feb49b365f67097ddf7b20b754f4ae79480c
beambeambeam <supawitmarayat@gmail.com>
2026-01-01 23:11:15 +0700

    refactor: remove unused components and clean up routes

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -5,9 +5,7 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
 
 import "../index.css";
 
@@ -46,12 +44,10 @@ function RootComponent() {
         storageKey="vite-ui-theme"
       >
         <div className="grid h-svh grid-rows-[auto_1fr]">
-          <Header />
           <Outlet />
         </div>
-        <Toaster richColors />
       </ThemeProvider>
-      <TanStackRouterDevtools position="bottom-left" />
+      {import.meta.env.DEV && <TanStackRouterDevtools position="bottom-left" />}
     </>
   );
 }
