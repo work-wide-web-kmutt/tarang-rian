368dbb76a548f892d61da8630b6445a0cb546197
beambeambeam <supawitmarayat@gmail.com>
2026-01-09 14:21:07 +0700

    feat: add GEN placeholder for dev

--- apps/web/src/course/schema.ts
+++ apps/web/src/course/schema.ts
@@ -3,18 +3,18 @@ import { z } from "zod";
 export const GenElectiveOptionSchema = z.object({
   code: z.string(),
   name: z.string(),
-  day: z.enum([
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ]),
   class: z.array(
     z.object({
       group: z.string(),
+      day: z.enum([
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ]),
       start: z.string().regex(/^\d{2}:\d{2}$/),
       end: z.string().regex(/^\d{2}:\d{2}$/),
     })
