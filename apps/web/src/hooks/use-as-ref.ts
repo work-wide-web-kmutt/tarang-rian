db25967636932aa70984dba72c49b4f153066eaa
beambeambeam <supawitmarayat@gmail.com>
2026-01-15 12:34:13 +0700

    feat: add editable components

---
+++ apps/web/src/hooks/use-as-ref.ts
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+import { useIsomorphicLayoutEffect } from "@/hooks/use-isomorphic-layout-effect";
+
+function useAsRef<T>(props: T) {
+  const ref = useRef<T>(props);
+
+  useIsomorphicLayoutEffect(() => {
+    ref.current = props;
+  });
+
+  return ref;
+}
+
+export { useAsRef };
