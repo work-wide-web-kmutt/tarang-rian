9974f46481b0824d035243b465e5eba7176a4b5f
beambeambeam <supawitmarayat@gmail.com>
2026-01-18 00:12:09 +0700

    feat: normalize instructor data structure in course collections

--- apps/web/src/course/schema.ts
+++ apps/web/src/course/schema.ts
@@ -19,7 +19,9 @@ export const GenElectiveOptionSchema = z.object({
       ]),
       start: z.string().regex(/^\d{2}:\d{2}$/),
       end: z.string().regex(/^\d{2}:\d{2}$/),
-      instructor: z.string(),
+      instructor: z
+        .array(z.string())
+        .min(1, "At least one instructor is required"),
     })
   ),
 });
